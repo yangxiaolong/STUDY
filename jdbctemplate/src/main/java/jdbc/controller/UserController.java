84af1b8c17794b16fd5c7ce70e42ba9fadf12008
Dragon <626571494@qq.com>
2025-03-17 23:26:24 +0800

    jdbctemplate update

--- jdbctemplate/src/main/java/jdbc/controller/UserController.java
+++ jdbctemplate/src/main/java/jdbc/controller/UserController.java
@@ -25,37 +25,37 @@ public class UserController {
     }
 
     @RequestMapping("/OUT_REQUIRED_EXEC_IN_REQUIRES_NEW")
-    public String OUT_REQUIRED_EXEC_IN_REQUIRES_NEW(String userName, Integer age) {
+    public String OUT_REQUIRED_EXEC_IN_REQUIRES_NEW() {
         return userAggService.OUT_REQUIRED_EXEC_IN_REQUIRES_NEW(null, null) ? "success" : "fail";
     }
 
     @RequestMapping("/OUT_REQUIRED_IN_NESTED_EXCE")
-    public String OUT_REQUIRED_IN_NESTED_EXCE(String userName, Integer age) {
+    public String OUT_REQUIRED_IN_NESTED_EXCE() {
         return userAggService.OUT_REQUIRED_IN_NESTED_EXCE(null, null) ? "success" : "fail";
     }
 
     @RequestMapping("/OUT_REQUIRED_EXEC_IN_NESTED")
-    public String OUT_REQUIRED_EXEC_IN_NESTED(String userName, Integer age) {
+    public String OUT_REQUIRED_EXEC_IN_NESTED() {
         return userAggService.OUT_REQUIRED_EXEC_IN_NESTED(null, null) ? "success" : "fail";
     }
 
     @RequestMapping("/OUT_REQUIRED_IN_REQUIRED_EXCE")
-    public String OUT_REQUIRED_IN_REQUIRED_EXCE(String userName, Integer age) {
+    public String OUT_REQUIRED_IN_REQUIRED_EXCE() {
         return userAggService.OUT_REQUIRED_IN_REQUIRED_EXCE(null, null) ? "success" : "fail";
     }
 
     @RequestMapping("/OUT_REQUIRED_EXEC_IN_REQUIRED")
-    public String OUT_REQUIRED_EXEC_IN_REQUIRED(String userName, Integer age) {
+    public String OUT_REQUIRED_EXEC_IN_REQUIRED() {
         return userAggService.OUT_REQUIRED_EXEC_IN_REQUIRED(null, null) ? "success" : "fail";
     }
 
     @RequestMapping("/OUT_REQUIRED_IN_NOT_SUPPORTED_ININ_REQUIRED")
-    public String OUT_REQUIRED_IN_NOT_SUPPORTED_ININ_REQUIRED(String userName, Integer age) {
+    public String OUT_REQUIRED_IN_NOT_SUPPORTED_ININ_REQUIRED() {
         return userAggService.OUT_REQUIRED_IN_NOT_SUPPORTED_ININ_REQUIRED(null, null) ? "success" : "fail";
     }
 
     @RequestMapping("/OUT_REQUIRED_EXEC_IN_NOT_SUPPORTED_ININ_REQUIRED")
-    public String OUT_REQUIRED_EXEC_IN_NOT_SUPPORTED_ININ_REQUIRED(String userName, Integer age) {
+    public String OUT_REQUIRED_EXEC_IN_NOT_SUPPORTED_ININ_REQUIRED() {
         return userAggService.OUT_REQUIRED_EXEC_IN_NOT_SUPPORTED_ININ_REQUIRED(null, null) ? "success" : "fail";
     }
 
