f9edfafc5ec7b8ce483d566911d460972a9609e4
Dragon <626571494@qq.com>
2025-03-20 23:10:13 +0800

    jdbctemplate update

--- jdbctemplate/src/main/java/jdbc/Application.java
+++ jdbctemplate/src/main/java/jdbc/Application.java
@@ -2,11 +2,13 @@ package jdbc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author yangxiaolong
  */
 @SpringBootApplication
+@EnableAsync
 public class Application {
 
     public static void main(String[] args) {
